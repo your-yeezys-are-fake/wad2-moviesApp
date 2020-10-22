9f13c6afc6d409c36e0d5cf1ed5fee885476a16e
Georgia Swindley <62120557+your-yeezys-are-fake@users.noreply.github.com>
2020-10-22 14:10:10 +0100

    Added routing and live data from API

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -51,6 +51,16 @@ export default ({ movie }) => {
           </li>
         ))}
       </ul>
+      <ul className="list-group list-group-horizontal">
+      <li key="pch" className="list-group-item list-group-item-dark">
+        Production Countries
+        {movie.production_countries.map(pcountries =>(
+          <li key={pcountries.name} className="list-group-item">
+            {pcountries.name}
+        </li>
+        ))} 
+        </li>
+      </ul>
     </>
   );
 };
