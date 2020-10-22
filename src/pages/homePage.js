535453323904da31955316f1a38ffa6d435a3201
Georgia Swindley <62120557+your-yeezys-are-fake@users.noreply.github.com>
2020-10-22 14:53:34 +0100

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,60 +1,32 @@
-import React, {useState, useEffect } from "react";
-import Header from "../components/headerMovieList";
-import MovieList from "../components/movieList";
-import FilterControls from "../components/filterControls";
+import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
-
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 
 const MovieListPage = () => {
-  const [titleFilter, setTitleFilter] = useState("");     
-  const [genreFilter, setGenreFilter] = useState("0"); 
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(movies => {
-        setMovies(movies);
-      });
-  }
-  , []);
-
-  const genre = Number(genreFilter)
-  let displayedMovies = movies
-    .filter(m => {
-      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
-    })
-    .filter(m => {
-      return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+    getMovies().then(movies => {
+      setMovies(movies);
     });
-
-  const handleChange = (type, value) => {
-    if (type === "name") setTitleFilter(value);
-    else setGenreFilter(value);
-  };
+  }, []);
 
   const addToFavorites = movieId => {
-    const index = movies.map(m => m.id).indexOf(movieId)
+    setMovies(movies => {
+      const index = movies.map(m => m.id).indexOf(movieId);
+      StubAPI.add(movies[index]);
+      let newMoviesState = [...movies]
+      newMoviesState.splice(index, 1);
+      return newMoviesState;
+    });
+  };
 
-    StubAPI.add(movies[index])
-    const updatedList = [...movies]  
-    updatedList.splice(index, 1)     
-    setMovies(updatedList)  
-  }
   return (
-    <>
- <Header numMovies={displayedMovies.length} />
-      <FilterControls onUserInput={handleChange} />
-      <MovieList
-        movies={displayedMovies}
+      <PageTemplate
+        title='Discover Movies'
+        movies={movies}
         buttonHandler={addToFavorites}
-      />              
-    </>
+      />
   );
 };
 
